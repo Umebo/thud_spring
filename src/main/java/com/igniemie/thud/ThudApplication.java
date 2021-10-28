8b9aec020b5c7aeb9c76700b5335fc40aa2e255c
igor <igorniemiec00@gmail.com>
2021-10-28 21:46:53 +0200

    Initial commit

---
+++ src/main/java/com/igniemie/thud/ThudApplication.java
@@ -0,0 +1,13 @@
+package com.igniemie.thud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/igniemie/thud/ThudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igniemie.thud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

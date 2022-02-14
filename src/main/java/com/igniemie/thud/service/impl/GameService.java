a5b93792eb80fa66c934494576f46305818f232c
igor <igorniemiec00@gmail.com>
2022-02-14 13:29:31 +0100

    winner checking added

--- src/main/java/com/igniemie/thud/service/impl/GameService.java
+++ src/main/java/com/igniemie/thud/service/impl/GameService.java
@@ -6,6 +6,7 @@ import com.igniemie.thud.model.Game;
 import com.igniemie.thud.database.dto.GamePlay;
 import com.igniemie.thud.model.GameStatus;
 import com.igniemie.thud.model.Player;
+import com.igniemie.thud.model.PlayerType;
 import com.igniemie.thud.service.IGameService;
 import com.igniemie.thud.session.GameSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -70,7 +71,39 @@ public class GameService implements IGameService {
         int[][] board = this.gameSession.getBoard();
         board[gamePlay.getDimX()][gamePlay.getDimY()] = gamePlay.getType().getValue();
         this.gameSession.setBoard(board);
+
+        if (checkWinner(board,PlayerType.X)) {
+            this.gameSession.setWinner(PlayerType.X);
+        } else if (checkWinner(board,PlayerType.O)) {
+            this.gameSession.setWinner(PlayerType.O);
+        }
+
         return board;
+    }
+
+    private Boolean checkWinner(int[][] board, PlayerType playerType) {
+        int[] boardArray = new int[9];
+        int counterIndex = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                boardArray[counterIndex] = board[i][j];
+                counterIndex++;
+            }
+        }
+
+        int[][] winCombinations = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+        for (int i = 0; i < winCombinations.length; i++) {
+            int counter = 0;
+            for (int j = 0; j < winCombinations[i].length; j++) {
+                if (boardArray[winCombinations[i][j]] == playerType.getValue()) {
+                    counter++;
+                    if (counter == 3) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
 }
